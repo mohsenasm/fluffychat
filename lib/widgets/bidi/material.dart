e08ab728554fc94f166d101bf78d7e4a52a6039e
Mohammad-Mohsen <m.m.asemanmanzar@gmail.com>
2026-07-16 19:25:30 +0330

    feat: extending matrial's TextField class to support bidi

--- lib/widgets/bidi/material.dart
+++ lib/widgets/bidi/material.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart' as material;
 import 'package:intl/intl.dart' as intl;
 
-export 'package:flutter/material.dart' hide Text;
+export 'package:flutter/material.dart' hide Text, TextField;
 
 extension BidiFormatter on String {
   String get bidiFormatted {
@@ -82,4 +82,85 @@ class Text extends material.Text {
   }
 }
 
-// TODO: also define new classes for: RichText, TextField, TextFormField, Linkify, SelectableLinkify
+class TextField extends material.TextField {
+  const TextField({
+    super.key,
+    super.groupId,
+    super.controller,
+    super.focusNode,
+    super.undoController,
+    super.decoration,
+    super.keyboardType,
+    super.textInputAction,
+    super.textCapitalization,
+    super.style,
+    super.strutStyle,
+    super.textAlign,
+    super.textAlignVertical,
+    super.textDirection,
+    super.readOnly,
+    super.showCursor,
+    super.autofocus,
+    super.statesController,
+    super.obscuringCharacter,
+    super.obscureText,
+    super.autocorrect,
+    super.smartDashesType,
+    super.smartQuotesType,
+    super.enableSuggestions,
+    super.maxLines,
+    super.minLines,
+    super.expands,
+    super.maxLength,
+    super.maxLengthEnforcement,
+    super.onChanged,
+    super.onEditingComplete,
+    super.onSubmitted,
+    super.onAppPrivateCommand,
+    super.inputFormatters,
+    super.enabled,
+    super.ignorePointers,
+    super.cursorWidth,
+    super.cursorHeight,
+    super.cursorRadius,
+    super.cursorOpacityAnimates,
+    super.cursorColor,
+    super.cursorErrorColor,
+    super.selectionHeightStyle,
+    super.selectionWidthStyle,
+    super.keyboardAppearance,
+    super.scrollPadding,
+    super.dragStartBehavior,
+    super.enableInteractiveSelection,
+    super.selectAllOnFocus,
+    super.selectionControls,
+    super.onTap,
+    super.onTapAlwaysCalled,
+    super.onTapOutside,
+    super.onTapUpOutside,
+    super.mouseCursor,
+    super.buildCounter,
+    super.scrollController,
+    super.scrollPhysics,
+    super.autofillHints,
+    super.contentInsertionConfiguration,
+    super.clipBehavior,
+    super.restorationId,
+    super.stylusHandwritingEnabled,
+    super.enableIMEPersonalizedLearning,
+    super.enableInlinePrediction,
+    super.contextMenuBuilder,
+    super.canRequestFocus,
+    super.spellCheckConfiguration,
+    super.magnifierConfiguration,
+    super.hintLocales,
+  });
+
+  @override
+  material.TextDirection? get textDirection {
+    return controller?.text.textDirection;
+  }
+}
+
+
+// TODO: also define new classes for: RichText, TextFormField, Linkify, SelectableLinkify
