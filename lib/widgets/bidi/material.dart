e1b552b4b311be87f817a2d5b5708d8adc0ab55f
Mohammad-Mohsen <m.m.asemanmanzar@gmail.com>
2026-07-17 00:22:44 +0330

    cleaning

--- lib/widgets/bidi/material.dart
+++ lib/widgets/bidi/material.dart
@@ -1,5 +1,3 @@
 export 'package:fluffychat/widgets/bidi/text.dart';
 export 'package:fluffychat/widgets/bidi/textfield.dart';
 export 'package:flutter/material.dart' hide Text, TextField;
-
-// TODO: also define new classes for: Linkify, SelectableLinkify
